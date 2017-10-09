6647c129cd9d4ea446008c03513c97dd40368a77
Arnaud Soler <arnaus17@student.uia.no>
2017-10-09 02:44:40 +0200

    publishing my app

---
+++ greeter.js
@@ -0,0 +1,12 @@
+
+var myGreetings = new Array()
+
+myGreetings[0] = "Hi !";
+myGreetings[1] = "Hello !";
+myGreetings[2] = "Bonjour !";
+myGreetings[3] = "Hola !";
+myGreetings[4] = "Hei hei !";
+myGreetings[5] = "Hallo !";
+myGreetings[6] = "Salut !";
+
+console.log(myGreetings[Math.floor((Math.random() * 6))]);
